13b4279b46ad4af44388407da2b0d9fb7ef6b187
Fuad Abdella <v-fabdella@microsoft.com>
2021-06-16 04:38:48 -0700

    Got the add schedule to work on the blockchain and also show up on the UI

--- pages/Mentor.jsx
+++ pages/Mentor.jsx
@@ -15,16 +15,54 @@ const Mentor = ({ account, decentee }) => {
     const [contractAddresss, setContractAddresss ] = useState("");
     const [mentorContractAddress, setMentorContractAddress ] = useState("")
     const [projectState, setProjectState] = useState(null)
-    const [popoverOpen, setPopoverOpen] = useState(false);
+    const [scheduleModal, setScheduleModal] = useState(null)
+//========================================================================
+    const [shortCode, setShortCode] = useState("")
+    const [modalDescription, setModalDescription] = useState("")
+    const [ethValue, setEthValue] = useState("")
+
+
+
+   // const [popoverOpen, setPopoverOpen] = useState(false);
 
     //console.log("contract", contractAddresss)
     
-    const toggle = () => setPopoverOpen(!popoverOpen);
-    const getContractAddress = (e) => {
+    // const toggle = () => setPopoverOpen(!popoverOpen);
 
-      setMentorContractAddress(e.target.value)
+//==============================================================================================================================
+const getContractAddress = (e) => {
+  
+  setMentorContractAddress(e.target.value)
 
-    }
+}
+
+
+
+//==============================================================================================================================
+    
+const onShortcode = (e) => {
+  
+  setShortCode(e.target.value)
+
+}
+
+//==============================================================================================================================
+    
+const onDescription = (e) => {
+  
+  setModalDescription(e.target.value)
+
+}
+
+//==============================================================================================================================
+    
+const onValue = (e) => {
+  
+  setEthValue(e.target.value)
+
+}
+
+
 
 //==============================================================================================================================
 
@@ -43,31 +81,64 @@ const btnGo = () => {
 
 
 
-
+//==============================================================================================================================
 
 
  const utilProjectStatus = function(statusCode) {
     //initiated, accepted, closed
-    console.log(statusCode);
-    this.uiLblProjectState = document.getElementById("lbl-project-status");
+    //console.log(statusCode);
+    var uiLblProjectState = document.getElementById("lbl-project-status");
     switch(statusCode){
       case 0:
-        this.uiLblProjectState.classList.add('bg-primary');
-        this.uiLblProjectState.textContent = "Initiated";
+        uiLblProjectState.classList.add('bg-primary');
+        setProjectState("Initiated");
         break;
       case 1:
-        this.uiLblProjectState.classList.add('bg-success');
-        this.uiLblProjectState.textContent = "Accepted";
+        uiLblProjectState.classList.add('bg-success');
+        setProjectState("Accepted");
         break;
       case 2:
-        this.uiLblProjectState.classList.add('bg-warning');
-        this.uiLblProjectState.textContent = "Closed";
+        uiLblProjectState.classList.add('bg-warning');
+        setProjectState("Closed");
         break;
     }
   }
 
-  //==============================================================================================================================
+//==============================================================================================================================
+
+
+
+
   
+const utilGetEthValue = async () => {
+    var uiLblTotalEth = document.getElementById("lbl-total-eth");
+    var uiLblDisbursedEth = document.getElementById("lbl-disbursed-eth");
+
+    let totalRow;
+    let totalDisbursed=0;
+    let totalValue = 0;
+
+    await decentee.methods.totalSchedules().call().then((result) => {
+      totalRow = result;
+    });
+
+    for (let i=0; i<= totalRow-1; i++){
+      await decentee.methods.scheduleRegister(i).call().then((result)=>{
+        totalValue += result["value"]/1000000000000000000;
+        if (result["scheduleState"] == 4){
+          totalDisbursed += result["value"]/1000000000000000000;
+        }
+      });
+    }
+
+    uiLblTotalEth.innerHTML = Math.round(totalValue*100)/100;
+    uiLblDisbursedEth.innerHTML = Math.round(totalDisbursed*100)/100;
+  }
+  
+
+
+
+//==============================================================================================================================
   const deployFreelancer = function() {
     const web3 = window.web3;
 
@@ -92,7 +163,7 @@ const btnGo = () => {
       console.log("error", account);            
     })
     .on('receipt', (receipt) => {
-      console.log("DONE " + receipt.contractAddress); // contains the new contract address
+      //console.log("DONE " + receipt.contractAddress); // contains the new contract address
       setContractAddresss(receipt.contractAddress)
       
       
@@ -106,16 +177,16 @@ const btnGo = () => {
       
       var newFreelancerContract = new web3.eth.Contract( DecenteeArtifact.abi, freelanceContractAddress);
 
-      console.log( newFreelancerContract)
+      //console.log( newFreelancerContract)
 
       newFreelancerContract.methods.decenteeAddress().call().then((result) =>{
         setMentorContractAddress(result)
-        console.log(result)
+      //  console.log(result)
         uiLblFreelancerAddress = result;
       });
       
       newFreelancerContract.methods.projectState().call().then((result) =>{
-        // this.utilProjectStatus(0);
+        utilProjectStatus(0);
       });
       
       //update the ETH Value boxes
@@ -177,45 +248,143 @@ const btnGo = () => {
 
 //==============================================================================================================================
 
-
-
-
-
-
-// const btnAddSchedule = async () =>{
-//   if (document.getElementById("Schedule-Form").checkValidity()){
-//     this.uiTxtShortCode = document.getElementById("txt-short-code").value;
-//     this.uiTxtScheduleDescription = document.getElementById("txt-schedule-description").value;
-//     this.uiTxtScheduleValue = document.getElementById("txt-schedule-value").value;
-//     this.uiSpnAddSchedule = document.getElementById("spn-add-schedule");
-//     this.uiSpnAddSchedule.classList.remove('d-none');
-//     this.utilToggerAllButtonOnOff(0);
-    
-//     this.freelancerContract.methods.addSchedule(this.uiTxtShortCode, this.uiTxtScheduleDescription, App.web3.utils.toWei(this.uiTxtScheduleValue, 'ether')).send({from: this.account})
-//     .on('error', function(error, receipt) { 
-//       console.log(App.uiSpnAddSchedule);
-//       App.uiSpnAddSchedule.classList.add('d-none');
-//       App.scheduleModal = Modal.getInstance(document.getElementById('scheduleModal'));
-//       App.scheduleModal.hide();
-//       App.utilToggerAllButtonOnOff(1);
-//     })
-//     .then((result) =>{
-//       console.log("try to do this");
-//       this.uiTblScheduleTable = document.getElementById("tbl-schedule-table");
-//       this.uiTblScheduleTable.classList.remove('d-none');  
-//       this.uiSpnAddSchedule.classList.add('d-none');
-//       this.scheduleModal = Modal.getInstance(document.getElementById('scheduleModal'));
-//       this.utilAddScheduleToTable(this.uiTxtShortCode, this.uiTxtScheduleDescription, App.web3.utils.toWei(this.uiTxtScheduleValue, 'ether'), 0);
-//       this.utilGetEthValue();
-//       this.scheduleModal.hide();
-//       this.utilToggerAllButtonOnOff(1);
-//     });
-
+// const utilToggerAllButtonOnOff = function(state){
+//   if (state == 0){
+//     let buttons = document.getElementsByTagName("button");
+//     let i;
+//     for (i = 0; i < buttons.length; i++) {
+//       buttons[i].classList.add("disabled");
+//       console.log(buttons[i]);
+//     }
 //   }
-//   else{
-//     console.log("nope");
+//   else if (state == 1){
+//     let buttons = document.getElementsByTagName("button");
+//     let i;
+//     for (i = 0; i < buttons.length; i++) {
+//       buttons[i].classList.remove("disabled");
+//       console.log(buttons[i]);
+//     }
 //   }
 // }
+
+
+const utilScheduleState = (stateCode) => {
+  //planned, funded, started, approved, released
+  switch(stateCode){
+    case 0:
+      return "<span class='badge bg-primary'>Planned</span>";
+    case 1:
+      return "<span class='badge bg-success'>Funded</span>";
+    case 2:
+      return "<span class='badge bg-warning'>Started</span>";
+    case 3:
+      return "<span class='badge bg-info'>Approved</span>";
+    case 4:       
+      return "<span class='badge bg-dark'>Released</span>";
+  }
+}
+
+
+const utilAddScheduleToTable = (shortcode, description, value, state, action, scheduleID = 0) => {
+
+  let tr;
+  let td; 
+
+  var uiTblScheduleTableBody = document.getElementById("schedule-table-body");    
+  tr = document.createElement("tr");
+  td = document.createElement("td");
+  td.innerHTML = shortcode;
+  tr.appendChild(td);
+  uiTblScheduleTableBody.appendChild(tr);
+
+  td = document.createElement("td");
+  td.innerHTML = description;
+  tr.classList.add("table-active");
+  tr.appendChild(td);
+  uiTblScheduleTableBody.appendChild(tr);
+
+  td = document.createElement("td");
+  td.innerHTML = value/1000000000000000000;
+  td.classList.add('text-end');
+  tr.appendChild(td);
+
+  td = document.createElement("td");
+  td.innerHTML = utilScheduleState(parseInt(state));
+  tr.appendChild(td);
+
+  td = document.createElement("td");
+  //let client fund this if (a) it's the client (b) schedule is not funded (c) project is accepted
+  if (action === "client" && state == 0 && this.freelancerContractStatus == 1){
+    td.innerHTML = '<button type="button" onclick="App.btnFundSchedule('+ value/1000000000000000000 + ',' + scheduleID + ')" class="btn btn-primary btn-sm">Fund</button>';
+    td.innerHTML += '<span class="spinner-border spinner-border-sm d-none" role="status" id="spn-schedule-action-'+ scheduleID + '"></span>';
+  }
+  else if (action === "freelancer" && state == 1 && this.freelancerContractStatus == 1){
+    td.innerHTML = '<button type="button" onclick="App.btnStartSchedule(' + scheduleID + ')" class="btn btn-success btn-sm">Start Work</button>';
+    td.innerHTML += '<span class="spinner-border spinner-border-sm d-none" role="status" id="spn-schedule-action-'+ scheduleID + '"></span>';
+  }
+  else if (action === "client" && state == 2 && this.freelancerContractStatus == 1){
+    td.innerHTML = '<button type="button" onclick="App.btnApproveSchedule(' + scheduleID + ')" class="btn btn-warning btn-sm">Approve</button>';
+    td.innerHTML += '<span class="spinner-border spinner-border-sm d-none" role="status" id="spn-schedule-action-'+ scheduleID + '"></span>';
+  }
+  else if (action === "freelancer" && state == 3 && this.freelancerContractStatus == 1){
+    td.innerHTML = '<button type="button" onclick="App.btnReleaseFunds(' + scheduleID + ')" class="btn btn-info btn-sm">Release Funds</button>';
+    td.innerHTML += '<span class="spinner-border spinner-border-sm d-none" role="status" id="spn-schedule-action-'+ scheduleID + '"></span>';
+  }
+
+  tr.appendChild(td);
+
+  uiTblScheduleTableBody.appendChild(tr);
+}
+
+
+
+
+
+const btnAddSchedule = async () =>{
+  const web3 = window.web3;
+  if (document.getElementById("Schedule-Form").checkValidity()){
+    // var uiTxtShortCode = document.getElementById("txt-short-code").value;
+
+    // var uiTxtScheduleDescription = document.getElementById("txt-schedule-description").value;
+
+    // var uiTxtScheduleValue = document.getElementById("txt-schedule-value")
+
+    var freelancerContract = decentee;
+ 
+    var uiSpnAddSchedule = document.getElementById("spn-add-schedule");
+        uiSpnAddSchedule.classList.remove('d-none');
+   // this.utilToggerAllButtonOnOff(0);
+   console.log(freelancerContract.methods)
+
+    freelancerContract.methods.addSchedule(shortCode, modalDescription, web3.utils.toWei(ethValue, 'ether')).send({from: account})
+    .on('error', function(error, receipt) {
+
+      console.log("test ", error);
+
+      uiSpnAddSchedule.classList.add('d-none');
+
+    // var scheduleModal = Modal.getInstance(document.getElementById('scheduleModal'));
+    //     scheduleModal.hide();
+      // utilToggerAllButtonOnOff(1);
+    })
+    .then((result) =>{
+      console.log("try to do this");
+      var uiTblScheduleTable = document.getElementById("tbl-schedule-table");
+          uiTblScheduleTable.classList.remove('d-none');  
+          uiSpnAddSchedule.classList.add('d-none');
+     // var scheduleModal = Modal.getInstance(document.getElementById('scheduleModal'));
+
+        utilAddScheduleToTable(shortCode, modalDescription, web3.utils.toWei(ethValue, 'ether'), 0);
+        utilGetEthValue();
+        //  scheduleModal.hide();
+      // this.utilToggerAllButtonOnOff(1);
+    });
+
+  }
+  else{
+    console.log("nope");
+  }
+}
 
 
 
@@ -271,7 +440,7 @@ const btnGo = () => {
             <li className="list-group-item"><span className="fw-bold">Contract Address: {contractAddresss} </span><span id="lbl-contract-address"></span></li>
             <li className="list-group-item"><span className="fw-bold">Instructors Address: {mentorContractAddress} </span><span id="lbl-freelancer-address"></span></li>
             <li className="list-group-item"><span className="fw-bold">Client's Wallet: </span><span id="lbl-client-address" /></li>
-            <li className="list-group-item"><span className="fw-bold">Coaching-Date: </span><span className="badge" id="lbl-project-status"></span></li>
+            <li className="list-group-item"><span className="fw-bold">Project State: {projectState}</span><span className="badge" id="lbl-project-status"></span></li>
           </ul>
         </div>
         <div className="col-3">
@@ -324,27 +493,27 @@ const btnGo = () => {
             <h5 className="modal-title" id="scheduleModalLabel">New Schedule</h5>
             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          {/* <form id="Schedule-Form" >
+           <form id="Schedule-Form" >
           <div className="modal-body">
               <div className="mb-3">
                 <label htmlFor="txt-short-code" className="col-form-label">Short Code:</label>
-                <input type="text" className="form-control" placeholder="Enter short code e.g. DEV" value="UAT" id="txt-short-code" required />
+                <input type="text" className="form-control" placeholder="Enter short code e.g. DEV" onChange={onShortcode} id="txt-short-code" required />
               </div>
               <div className="mb-3">
                 <label htmlFor="txt-schedule-description" className="col-form-label">Description:</label>
-                <input type="text" className="form-control" placeholder="Enter description e.g. Development Stage" value="User Acceptance Testing" id="txt-schedule-description" required />
+                <input type="text" className="form-control" placeholder="Enter description e.g. Development Stage" onChange={onDescription} id="txt-schedule-description" required />
               </div>
               <div className="mb-3">
                 <label htmlFor="txt-schedule-value" className="col-form-label">Value</label>
-                <input type="number" min="0" step="any" className="form-control" id="txt-schedule-value" value="1.5" placeholder="Enter value (in ETH) to be paid when done" required />
+                <input type="number" min="0" step="any" className="form-control" id="txt-schedule-value" onChange={onValue}  placeholder="Enter value (in ETH) to be paid when done" required />
               </div>
           </div>
+        </form> 
           <div className="modal-footer">
             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="submit" className="btn btn-primary" id="btn-Add-Schedule">Go</button>
+            <button type="submit" className="btn btn-primary" id="btn-Add-Schedule" onClick={btnAddSchedule}>Go</button>
             <div className="spinner-border spinner-border-sm d-none" role="status" id="spn-add-schedule"></div>
           </div>
-        </form> */}
         </div>
       </div>
     </div>
