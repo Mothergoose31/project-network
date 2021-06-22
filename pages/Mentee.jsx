a1a8f41f20223c93bee28e927dd2db6c8672eb09
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2021-06-22 10:26:43 -0700

    started Mentee page

--- pages/Mentee.jsx
+++ pages/Mentee.jsx
@@ -21,12 +21,30 @@ function Mentee() {
     // });
 
     return (
-        <div>
-            <h1>
-                hello
-            </h1>
-            
-        </div>
+        <body>
+            <div className="p-1 mb-1 bg-dark bg-gradient text-white rounded-3">
+                <div ClassName="container-fluid py-3">
+                    <h1 class="display-7 fw-bold">Client Smart Contract</h1>
+                    <p class="col-md-8 fs-4">This is the client's Distributed App</p>
+                    <div className="row">
+                        <div className="col-8">
+                            <div className="input-group input-group-lg">
+                                <input type="text" class="form-control" placeholder="Enter contract address" id="txt-contract-address"></input>
+                            </div>
+                        </div>
+                        <div className="col-4">
+                        <button class="btn btn-primary btn-lg" onclick="App.btnGoClient()" 
+                            type="button" id="btn-Deploy"
+                            data-bs-toggle="popover" title="Error" 
+                            data-bs-content="Smart Contract Not Found"
+                            data-bs-trigger="manual">
+                                Go
+                        </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </body>
     )
 }
 
