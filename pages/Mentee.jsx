d6426a6894886e13550820482a14157fdec06cc7
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2021-06-23 10:54:24 -0700

    Updated Mentee Page... more work needs to be done

--- pages/Mentee.jsx
+++ pages/Mentee.jsx
@@ -21,9 +21,15 @@ function Mentee() {
     // });
 
     return (
+        // Note from Joseph
+        // Mentee and Mentor page share the div withe the id container-fluid py-2 d-none
+        // later on this can be simplified, for now Im just writing out the page.
+        
         <body>
+
+    
             <div className="p-1 mb-1 bg-dark bg-gradient text-white rounded-3">
-                <div ClassName="container-fluid py-3">
+                <div className="container-fluid py-3">
                     <h1 class="display-7 fw-bold">Client Smart Contract</h1>
                     <p class="col-md-8 fs-4">This is the client's Distributed App</p>
                     <div className="row">
@@ -40,10 +46,45 @@ function Mentee() {
                             data-bs-trigger="manual">
                                 Go
                         </button>
+                        <div className="spinner-border spinner-border-sm d-none" role="status" id="spn-load">
+                            <span className="sr-only"></span>
+                        </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <div className="container-fluid py-2 d-none" id="con-contract">
+                <div className="row">
+                    <div className="col-6">
+                        <ul className="list-group">
+                            <li className="list-group-item"><span className="fw-bold">Adress:</span><span id="lbl-contract-address"></span></li>
+                            <li className="list-group-item"><span className="fw-bold">Mentors-Wallet:</span><span id="lbl-freelancer-address"></span></li>
+                            <li className="list-group-item"><span className="fw-bold">Mentee's Wallet:</span><span id="lbl-client-address"></span></li>
+                            <li className="list-group-item"><span className="fw-bold">Project State:</span><span id="lbl-project-status"></span></li>
+                        </ul>
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <div className="card-header fw-bold text-center">Total Value (ETH)</div> 
+                            <div className="card-body">
+                                <p className="card-text text-center"><span className="fs-1" id="lbl-total-eth"></span></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-3">
+                    <div className="card">
+                        <div className="card-header fw-bold text-center">Disbursed (ETH)</div>
+                        <div className="card-body">
+                            <p className="card-text text-center"><span className="fs-1" id="lbl-disbursed-eth"></span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <br/>
+            
+
         </body>
     )
 }
